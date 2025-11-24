883e6af6e00f4ca1f7b927a34bc467738272e1e8
mia <artthou.reinhardt@gmail.com>
2025-11-23 23:32:08 -0500

    pls pls

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,30 +9,58 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
 
         // Collect character name from user.
-        System.out.print("Name: ");
+        System.out.println("Name: ");
         String name = scanner.nextLine();
 
-        // Collect character role from user.
-        System.out.print("Role: ");
-        String role = scanner.nextLine();
+        System.out.println("Your name is " + name + " and your role is a delivery driver.");
 
-        System.out.println("Your name is " + name + " and your role is " + role + ".");
-        
-        // TODO Create character by collecting user input (name + role.
+        // Start the adventure. 
+        printDramaticText("It's your first day on the job! You're a recent hire at NomNom Express.");
 
-        // TODO Print character sheet.
+        int ordernumber = (int)(Math.random() * 900000) + 100000;
 
-        // Start the adventure.
-        printDramaticText("Our adventure begins in a shady tavern ...");
+        System.out.println("The order number is " + ordernumber + "."); // prints 7 random digits
+
+        System.out.println("Where the order is coming from: ");
+        String place = scanner.nextLine();
+
 
         // Roll a d20
-        System.out.print("Press Enter to roll a d20.");
+        System.out.println("Press Enter to see how many items are in the order."); // sees how far away the order is
         scanner.nextLine();
 
-        int roll = (int)(Math.random() * 20) + 1;
-        Ascii.drawD20(roll);
 
-        // TODO Continue ...
+        int roll = (int)(Math.random() * 20) + 1; // 1-20
+        Ascii.drawD20(roll);
+        if (roll == 1) { // rolls a 1 it can't be retrieved
+            System.out.println("This order does not meet the requirements to be delivered. Now you have to wait longer for the next one");
+        }
+        else {
+            System.out.println("This order is good to be picked up!"); 
+        }
+
+        System.out.println("Press enter to see how many minutes away are you from the pickup point?");
+        scanner.nextLine();
+        int[] badNum = {0,5,6,7,8,10,13,17,16,20,23,26,28,31,33,42,41,49,51,62}; // list of bad numbers to roll
+        int roll2 = (int)(Math.random() * 68); // 0-67
+        Ascii.drawD20(roll2);
+        for (int i = 0; i < badNum.length; i++){
+            if (roll2 == badNum[i]){
+                System.out.println("Chelsea stole your order! Oh no!?");
+                printDramaticText("The boss won't be happy about this...");
+            }
+        }
+        printDramaticText("You had a rough first day and the boss calls you to his office...");
+         int roll3 = (int)(Math.random() * 2) + 1;
+        System.out.println("Press enter to see if the boss fires you?");
+        scanner.nextLine(); 
+        Ascii.drawD20(roll3);
+        if (roll3 == 2){
+            System.out.println("You're fired! Guess you're back to eating ramen for breakfast, lunch, and dinner.");
+        }
+        else {
+            System.out.println("You're off the hook...for now.");
+        }
     }
         
     public static void printDramaticText(String text) {
